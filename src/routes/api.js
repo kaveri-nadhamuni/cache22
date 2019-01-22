eaa87a13e91db5c7c38d40e6faaab9d6f1341b0b
Junpeng Guo <junpe@HP-Spectre.mit.edu>
2019-01-22 00:24:43 -0500

    update

--- src/routes/api.js
+++ src/routes/api.js
@@ -6,5 +6,48 @@ const Creator = require('../models/creator');
 
 const router = express.Router();
 
+router.get('/user', function(req, res) {
+    //use req.query to get the passed in ID
+   
+    //User model to query Mongo User Collection and access that user object
+    //right method you should use in findOne({'_id' : PassedInIDHere})
+   
+    //return that object to the user 
+   
+    User.findOne({_id: req.query._id}, function(err, user){
+      res.send(user);
+    });
+});
+
+//add .get endpoint to access all stories written by specific user from database
+//insert function here
+
+
+
+
+router.get('/stories', function(req, res) {
+    Story.find({}, function(err, stories) {
+        res.send(stories);
+    });
+});
+   
+router.post('/story', connect.ensureLoggedIn(), function(req, res) {
+    const newStory = new Story({
+        'creator_id': req.user._id,
+        'creator_name': req.user.name,
+        'content': req.body.content,
+    });
+   
+    newStory.save(function(err,story) {
+        User.findOne({_id: req.user.id}, function(err,user) {
+            user.last_post = req.body.content;
+           user.save();
+        });
+        // configure socketio
+        if (err) console.log(err);
+    });
+   
+    res.send({});
+});
 
 module.exports = router;
