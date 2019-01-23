b7495585f8585a7cb6275b6782f3d6916e7941c2
Sophie Guo <sophiejguo@yahoo.com>
2019-01-23 13:59:47 -0500

    added dummy objects

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const UserModelSchema = new mongoose.Schema({
     creator_id: String,
+    googleid: String, //is creator_id same as googleid
     creator_name: String,
     bio: String,
     stories: String,
